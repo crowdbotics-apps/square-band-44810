5270775f011ac671b71815bc5e472b55a050fca3
Crowdbotics <team@crowdbotics.com>
2023-11-30 00:35:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-band-44810",
+  url: "https://square-band-44810.botics.co",
+  api: "https://square-band-44810.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
